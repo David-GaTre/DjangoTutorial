ffa7c5205ae77b3a3c9e6f7333d376e9ad1641fa
David Garcia <d.garcia@ukko.mx>
2021-06-25 07:12:41 -0500

    Third part

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
-    url('', views.index, name='index'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('/specifics/<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]

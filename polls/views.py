58b307f8c5be2b9c612b5bb22c0af90ab28b95fa
David Garcia <gtdavid2013@gmail.com>
2021-06-27 05:05:44 -0500

    Part 5

--- polls/views.py
+++ polls/views.py
@@ -13,13 +13,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
+    ...
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
